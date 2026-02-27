ec7e0141db0ddbe5feb72082aef976f8b5354395
lomesh2312 <lomeshsonkeshriya@gmail.com>
2026-02-28 01:23:39 +0530

    Implement feature importance analysis in training
    Added feature importance analysis and selection to model training.

--- property_price_prediction/train_model.py
+++ property_price_prediction/train_model.py
@@ -25,23 +25,25 @@ for col in binary_columns:
 
 df = pd.get_dummies(df, columns=["furnishingstatus"], drop_first=True)
 
-features = [
-    "area",
-    "bedrooms",
-    "guestroom",
-    "bathrooms",
-    "mainroad",
-    "prefarea",
-    "stories",
-    "parking",
-    "basement",
-    "airconditioning",
-    "furnishingstatus_semi-furnished",
-    "furnishingstatus_unfurnished"
-]
-
-X = df[features]
+X = df.drop("price", axis=1)
 y = df["price"]
+
+temp_rf = RandomForestRegressor(random_state=42)
+temp_rf.fit(X, y)
+
+importances = temp_rf.feature_importances_
+feature_names = X.columns
+
+feature_importance_df = pd.DataFrame({
+    "Feature": feature_names,
+    "Importance": importances
+}).sort_values(by="Importance", ascending=False)
+
+print("Feature Importance:\n", feature_importance_df)
+
+
+top_features = feature_importance_df["Feature"].head(8).values
+X = X[top_features]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
